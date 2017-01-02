78078d0499d1669897ca6392fe22f94c819bdc00
Shashank Srivastava <shashank.ssriv@gmail.com>
2017-01-02 12:42:09 +0530

    Update default.rb
    Updated the recipe with cleaner code. Also added file & service resources.

--- cookbooks/vagrant_book/recipes/default.rb
+++ cookbooks/vagrant_book/recipes/default.rb
@@ -1,5 +1,30 @@
 execute "apt-get update"
-package "haproxy"
-package "nfs-kernel-server"
-package "nfs-common"
-execute "service nfs-kernel-server start"
+package 'haproxy' do
+  action :install
+end
+
+cookbook_file "/etc/haproxy/haproxy.cfg" do
+  source "haproxy.cfg"
+end
+
+file '/etc/default/haproxy' do
+  content 'ENABLED=1'
+  mode '0755'
+  action :create
+end
+
+service 'haproxy' do
+  action [ :enable, :start ]
+end
+
+package 'nfs-kernel-server' do
+  action :install
+end
+
+service 'nfs-kernel-server' do
+  action [ :enable, :start ]
+end
+
+package 'nfs-common' do
+  action :install
+end
